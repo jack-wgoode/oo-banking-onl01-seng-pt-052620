7d5776ca589a7b12739003cc460e358dfa2eb60d
Jack Goode <jack.w.goode@icloud.com>
2020-06-26 00:06:28 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,3 +1,34 @@
 class Transfer
-  # your code here
+  
+  attr_accessor :sender, :receiver, :status, :amount, :balance
+  
+  @@all = []
+  
+  def initialize(account_name1, account_name2, amount)
+    @sender = account_name1
+    @receiver = account_name2
+    @status = "pending"
+    @amount = amount
+  end 
+  
+  def valid?
+    @sender.valid? && @receiver.valid? && self.status != "complete"
+  end
+  
+  def insufficient_funds?
+    self.valid && @sender.balance >= self.amount
+  end
+  
+  def execute_transaction 
+    if self.valid? && self.status == "pending"
+       self.sender.withdraw(amount)
+       self.receiver.deposit(amount)
+       self.status = "complete"
+       @@all << self
+     elsif self.insufficient_funds? 
+     self.status = "rejected"
+     "Transaction rejected. Please check your account balance."
+    end
+   end    
+  
 end
